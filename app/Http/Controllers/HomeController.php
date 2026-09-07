41ff39c07e5a0bb7a86774a9e57e14de3073f69a
diemildefreude <s.elliot.perez@gmail.com>
2026-09-08 05:39:19 +0700

    Set posts to be shown in descending order.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = Posts::orderBy('id', 'desc')->get();
         return view('main', ['posts' => $posts]);
     }
 }
